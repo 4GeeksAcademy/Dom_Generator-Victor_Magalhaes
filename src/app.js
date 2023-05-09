f124ce0c8fe1a12eb76ce5eea85dabd4746e95cc
VictorMagaCBT <126924738+VictorMagaCBT@users.noreply.github.com>
2023-05-09 23:46:04 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  document.getElementById("newDom").innerHTML = generateDom();
+};
+
+const generateDom = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extensions = [".com", ".net", ".pt", ".es"];
+
+  return (
+    pronoun[Math.floor(Math.random() * 2)] +
+    adj[Math.floor(Math.random() * 2)] +
+    noun[Math.floor(Math.random() * 2)] +
+    extensions[Math.floor(Math.random() * 4)]
+  );
+};
+
+
+
+/*window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
-};
+};*/
